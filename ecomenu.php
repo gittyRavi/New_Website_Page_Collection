531e54c8d63dc9831e13d7562a5a027e4949457c
gittyRavi <rairavisaxena@gmail.com>
2016-12-21 18:51:32 +0530

    Update ecomenu.php

--- ecomenu.php
+++ ecomenu.php
@@ -1,1 +1,217 @@
+<HTML>
+<HEAD>
 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+
+<TITLE>Eco</TITLE>
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">TEXT</button></pre>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<script src="jquery.archive_buttons.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">    
+	H1{background-color:pink;}
+	
+		button a
+				{
+					text-decoration:none;
+					color:white;
+				}
+	
+		.circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+				}
+				
+		.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: red;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<body>
+<H1 class="class3"><center>Ecology, Environment & Animals</center></H1>
+
+<z class="t" align="center">
+<center>
+<z class="blink_text"><u>Click on the tabs to know about the "Endangered Animals & Plants" in each category.</u></z>
+</center>
+</z>
+<br/>
+
+<center>
+<table>										 <!--table open-->
+
+<tr>											<!--row1-->
+<td>
+<button class="circle" style="background: red;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+<a href="new_look(webpage_bald_eagle).php">Eagle</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: violet;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.8em;color: white">
+<a href="new_look(webpage_gangetic_dolphin).php">Dolphin</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: red;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: violet;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.9em;color: white">
+
+</H3>
+</button>
+</td>
+</tr>
+
+<tr>											<!--row2-->
+<td>
+<button class="circle" style="background: pink;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+<a href="new_look(webpage_green_sea_turtle).php">Turtle</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: green;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.8em;color: white">
+<a href="new_look(webpage_snow_leopard).php">Leopard</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: pink;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: green;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.9em;color: white">
+
+</H3>
+</button>
+</td>
+</tr>
+
+<tr>											<!--row3-->
+<td>
+<button class="circle" style="background: green;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.85em;color: white"> 
+<a href="new_look(webpage_western_hoolock_gibbon).php">Gibbon</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: pink;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.9em;color: white"> 
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: green;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.9em;color: white"> 
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: pink;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:0.9em;color: white"> 
+
+</H3>
+</button>
+</td>
+</tr>
+
+<tr>											<!--row4-->
+<td>
+<button class="circle" style="background: violet;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+<a href="new_look(webpage_bengal_tiger).php">Tiger</a>
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: red;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: violet;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+
+</H3>
+</button>
+</td>
+<td>
+<button class="circle" style="background: red;">
+<H3 style="horizontal-align:center;vertical-align:center;text-align:center;padding:1px;font-size:1em;color: white">
+
+</H3>
+</button>
+</td>
+</tr>
+</table>									<!--table1 close-->
+</center>
+
+<H1> &nbsp </H1>
+
+<div id="footer" style="position:relative;bottom:0px">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+</BODY>
+</HTML>
