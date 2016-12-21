14b7a420b04e9b7ea684a4f7436a02e3852a35b5
gittyRavi <rairavisaxena@gmail.com>
2016-12-21 18:49:47 +0530

    Update jquery.buttons.js

--- jquery.buttons.js
+++ jquery.buttons.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
+	$("#home").click(function(){
+		window.location = "homepage.html";
+		return false;
+	});
+
+	$("#id1").click(function(){
+		window.location = "ecomenu.php";
+		return false;
+	});
+	
+	$("#id2").click(function(){
+		window.location = "archive.php";
+		return false;
+	});
+	
+	$("#id3").click(function(){
+		window.location = "new_look(webpage_bald_eagle).php";
+		return false;
+	});
+	
+	$("#id4").click(function(){
+		window.location = "contacts.php";
+		return false;
+	});
+});
 
