f0ad0b81b71474afaea0a87a56c2d1c3bfe908ae
gittyRavi <rairavisaxena@gmail.com>
2016-12-22 01:03:11 +0530

    Update jquery.archive_buttons.js

--- jquery.archive_buttons.js
+++ jquery.archive_buttons.js
@@ -1,1 +1,42 @@
+$(document).ready(function(){
+ 	$("#id5").click(function(){
+ 	 $("#id5").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+		function a(){window.location="learncplusplus.php";}
+		a();
+
+
+  });
+
+ 	$("#id6").click(function(){
+ 	 $("#id6").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+		function b(){window.location="learnjava.php";}
+		b();
+
+  });
+
+ 	$("#id7").click(function(){
+ 	 $("#id7").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+		function c(){window.location="ecomenu.php";}
+		c();
+
+  });
+
+ 	$("#id8").click(function(){
+ 	 $("#id8").animate({
+      		 left : '85%',
+      		 opacity :'0',
+      		},2000);
+
+
+  });
+ });
 
