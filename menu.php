882e971b0937097df0e3b0cfb9f2b8aa9697c95d
gittyRavi <rairavisaxena@gmail.com>
2016-12-28 23:17:43 +0530

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Menu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  .circle
+	{
+		height:2em;
+		width:2em;
+		border-radius:100%;
+		background-color:orange;
+		position:absolute;
+		margin:auto;
+	}
+	</style>
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+
+<button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="navbar-collapse">
+<span class="sr-only">Toggle Navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+
+<a class="navbar-brand" href="">Search The Menu <sup style="color:orange;">bootstrap page</sup></a>
+
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav navbar-right">
+
+<li><a href="#"><span class="glyphicon glyphicon-user"></span> ~About My Works</a></li>
+
+<li>
+<a href="homepage.html">Home</a>
+</li>
+
+<li class="active">
+<a href="menu.php">Menu</a>
+</li>
+
+<li class="dropdown">
+<a href="archive.php" class="dropdown-toggle" data-toggle="dropdown">Archive<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="archive.php">Archive:Main</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Learn</li>
+<li><a href="learncplusplus.php">C++</a></li>
+<li><a href="learnjava.php">Java</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Extras</li>
+<li><a href="ecomenu.php">Eco</a></li>
+<li><a href="search_video.html">Music</a></li>
+</ul>
+</li>
+
+<li>
+<a href="contacts.php">Contact</a>
+</li>
+
+</ul>
+</div>
+
+</nav>
+  
+<div class="container">
+  <h3>Hi friends, this is a new website created by me { रवि सक्सेना :) }</h3>
+  <p>I am eagerly waiting for your reviews as you can send me your messages through the <br/>contact tab link.</p>
+  <br/>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b>Here you can learn about basics of C++.</b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b>And you can learn about basics of Java.</b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b>Also you can browse through music videos and songs in database.</b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b>And search the Youtube videos.</b></pre>
+  <pre style="text-indent:2.5em;position: center center"><div class="circle" style="vertical-align:center;margin-top:-0.3em"></div><b>You can also learn about your Environment and send me your ideas for conservations.</b></pre>
+  
+</div>
+
+</body>
+</html>
