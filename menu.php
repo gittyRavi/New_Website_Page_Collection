8d6ded62079c3edaaff0527159eac144aa00361f
gittyRavi <rairavisaxena@gmail.com>
2016-12-28 23:30:53 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,6 +4,8 @@
 <head>
   <title>Menu</title>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Language" content="en">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
