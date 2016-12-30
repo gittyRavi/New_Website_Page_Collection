7fe6d9a41dd50a5564170d266fc92b9b38529b88
gittyRavi <rairavisaxena@gmail.com>
2016-12-30 21:20:07 +0530

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -9,29 +9,66 @@
 <meta name="description" content="A New Website.">
 <meta name="author" content="Ravi Saxena">
 
-<TITLE>Contact</TITLE>			<!--PHP-pg.2-->
-
-<div id="header">
-Search The Content<br/>			<!--Second PHP Page-->
-<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">TEXT</button>
-</pre>
-</div>
-
+<TITLE>
+Contact
+</TITLE>
+	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="jquery.buttons.js"></script>
 <link rel="stylesheet" type="text/css" href="style3.css">
-
-<Style type="text/CSS">
-	.t{color : Blue;}
-	.g{color:Red;}
-	.h{color:Green;}
-</Style>
-
+	
+<style>
+.container-fluid
+	{
+		width: 100%;
+		text-align: center;
+		> li {
+				float: none;
+				display: inline-block;
+			}
+	}
+.navbar-header
+	{
+		width: 100%;
+		text-align: center;
+		> li {
+				float: none;
+				display: inline-block;
+			}
+	}
+.navbar-brand {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 0;
+    text-align: center;
+    margin: auto;
+}
+.t{color : Blue;}
+.g{color:Red;}
+.h{color:Green;}
+.btn.btn-danger.navbar-btn{background:green;border:2px;position:relative;border-radius:25%;align:center;width:device-width;}
+.btn.btn-danger.navbar-btn:hover{background:skyblue;border:0px;position:relative;}
+input[type=submit]{border-radius:5%;}
+</style>
+	
 </HEAD>
-
 <BODY class="t">
-<div>						<!--div id="wrapper"-->
+<nav class="navbar navbar-inverse" style="background-color: #26a69a;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#" style="color:white;"><h5>Search The Content</h5></a>
+    </div><br/><br/>
+	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="home">HOME</button>
+	<button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id1">MENU</button>
+    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id2">ARCHIVE</button>
+    <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id3">My Artworks</button>
+  </div>
+</nav>
 
+<div class="container">
 <?php
 if (isset($_POST['contact_name']) && isset($_POST['contact_email']) && isset($_POST['contact_text']))
 {
@@ -68,19 +105,21 @@ if (isset($_POST['contact_name']) && isset($_POST['contact_email']) && isset($_P
 }
 ?>
 
-<form action="new 2.php" method="POST">					<!--html form linked to new 2.php-->
-	<center style="padding:2%">
+<form action="contacts.php" method="POST">					<!--html form linked to new 2.php-->
+	<center style="padding:0%">
 	Name:<br><input type="text" name="contact_name" style="border-radius:50%;text-align:center"><br><br>
 	Email Address:<br><input type="text" name="contact_email" style="width:200px;height:relative;border-radius:50%;text-align:center" ><br><br>
-	Message:<br><textarea name="contact_text" rows="6" cols="30" style="border-radius:10%;text-align:center"></textarea><br><br>
+	Message:<br><textarea name="contact_text" rows="3" cols="30" style="border-radius:10%;text-align:center"></textarea><br><br>
 	<input type="submit" value="Send">
 	</center>
 </form>
 
-<div id="footer">										<!--footer-->
-<button id="id4">CONTACT</button><br/><br/>
-<span style="font-size:0.7em">Dated 9/12/16.</span>
-</div>
-</div>
+</div><br>
+<footer class="container-fluid text-center" style="background-color: #004d40;color:white;padding:0%;bottom:0px;position:fixed;width:100%;">
+  <center>
+  <button class="btn btn-danger navbar-btn" style="border-radius:20%;" id="id4">Contact</button>
+  <h6>Dated 9/12/16.</h6>
+  </center>
+</footer>
 </BODY>
 </HTML>
