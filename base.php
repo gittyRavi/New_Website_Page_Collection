a770928e297572085c893088c145a42d1333bea8
gittyRavi <rairavisaxena@gmail.com>
2016-12-30 21:29:22 +0530

    Create base.php

---
+++ base.php
@@ -0,0 +1,212 @@
+<HTML>
+<HEAD>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A New Website.">
+<meta name="author" content="Ravi Saxena">
+
+<TITLE>World . Ecosystems</TITLE>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="jquery.buttons.js"></script>
+<link rel="stylesheet" type="text/css" href="style3.css">
+
+<Style type="text/CSS">
+	.t{color : Blue;}			
+	p{	-webkit-animation: my 10s infinite; /* Chrome, Opera, and Safari */
+		animation: my 10s infinite; /* Standard syntax */
+		background-color:red;
+	  }
+ 
+		@-webkit-keyframes my				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}
+ 
+		@keyframes my						/* Standard syntax */
+		{
+		0% {background-color: red;}
+		50% {background-color: yellow;}
+		100% {background-color: red;}
+		}      
+ 
+	H1{background-color:pink;}
+ 
+	html,body{
+		-webkit-animation: mymove 20s infinite; /* Chrome, Opera, and Safari */
+		animation: mymove 20s infinite; /* Standard syntax */
+		background-color: black;
+		height:100%;
+		}
+ 
+		@-webkit-keyframes mymove 				/* Chrome, Opera, and Safari */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}
+ 
+		@keyframes mymove						/* Standard syntax */
+		{
+		0% {background-color: black;}
+		50% {background-color: yellow;}
+		100% {background-color: black;}
+		}       
+		 .circle {
+			width: 50px;
+			height: 50px;
+			-webkit-border-radius: 25px;
+			-moz-border-radius: 25px;
+			border-radius: 25px;
+			background: red;
+				}
+			#anim{
+				width: 50px;
+				height: 50px;
+				background-color: red;
+				position: relative;
+				-webkit-animation: example 7s infinite; /* Chrome, Safari, Opera */
+														/* Chrome, Safari, Opera */
+				animation: example 7s infinite;
+				}
+
+														/* Chrome, Safari, Opera */
+			@-webkit-keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+			}
+
+														/* Standard syntax */
+			@keyframes example {
+				0%   {background-color:red; left:0px; top:0px;}
+				25%  {background-color:yellow; }
+				50%  {background-color:blue; }
+				75%  {background-color:green; }
+				100% {background-color:red; left:92%; top:0px;}
+				}
+			
+			p #g {
+						width: 100%;
+						height: 100%;
+						position: relative;
+				}
+			
+			center>div>img
+				{
+					border-radius:10%;
+					/*height:90px;*/
+				}
+				
+			div>img
+				{
+					border-radius:20%;
+					/*height:90px;*/
+				}
+			
+			.blink_text 											/* for blink operation. */
+				{
+						animation:2s blinker linear infinite;
+						-webkit-animation:2s blinker linear infinite;
+						-moz-animation:2s blinker linear infinite;
+
+						color: white;
+				}
+
+			@-moz-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@-webkit-keyframes blinker
+				{  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+
+			@keyframes blinker {  
+						0% { opacity: 1.0; }
+						50% { opacity: 0.0; }
+						100% { opacity: 1.0; }
+				}
+				
+			body
+				{
+						background-image:url("https://online.science.psu.edu/sites/default/files/biol011/Fig-15-Earth-Biomes.jpg");
+						background-width:device-width;
+				}
+</style>
+<link rel="stylesheet" type="text/CSS" href="mystyle.css">
+</HEAD>
+<BODY>
+
+<div id="wrapper">
+
+<div id="header">Search The Content<br/>
+<pre><button id="home">HOME</button>  <button id="id1">MENU</button>  <button id="id2">ARCHIVE</button>  <button id="id3">TEXT</button></pre>
+</div>
+
+<p class="t" align="center">
+<z class="blink_text">World Ecosystems</z>
+</p>
+
+<H1 class="class1" style="opacity:0.8;">Infos!!</H1>
+<p id="g" >
+<center>
+<div style="border-radius:15%;background-color:purple;width:90%;padding:3%;background: rgb(153, 0, 153);background: rgba(153, 0, 153, 0.6);"><img src="" alt="Green sea turtle" style="width:19.2%;height:relative;"><img src="" alt="Green sea turtle" style="width:19.2%;height:relative;"><img src="" alt="Green sea turtle" style="width:19.2%;height:relative;"><img src="" alt="Green sea turtle" style="width:19.2%;height:relative;"><img src="" alt="Green sea turtle" style="width:19.2%;height:relative;">
+</div>
+</center>
+</p>
+<H1 style="opacity:0.8;">
+<?php
+	echo " <H5 style='color:white'>Hello World!! this is php</H5>";
+?>
+</H1>
+<td>
+<H1 class="class7" style="opacity:0.8;">
+Western Hoolock Gibbon
+</H1>
+</td>
+<p style="opacity:0.7;">
+<!--<pre>-->
+<div style="background-color:brown;padding:5px;border-radius:5px;overflow:hidden;"><div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div><textarea maxlength="50" rows="7" cols="50" style="position:relative;max-width:100%;color:blue;background-color:yellow;font-family:cooper;font-weight:bold;box-shadow: 0 0 10px 10px pink; ">
+
+</textarea> <img src="" style="height:23%;width:relative"> <img src="" style="height:23%;width:relative"> <img src="" style="height:23%;width:relative"> <img src="" style="height:23%;width:relative"> <div style="background-color:orange;padding:5px;border-radius:5px;overflow:hidden"></div></div>
+<iframe width="relative" height="23%" src="https://www.youtube.com/embed/X14IQLk1xIk" frameborder="0" allowfullscreen></iframe></div>
+<!--</pre>-->
+</p>
+<div class="circle" id="anim">
+<pre>
+<H3 align="center"> 
+T=7s
+</H3>
+</pre>
+</div>
+<H2>
+<p class="t" align="center">Slide the Bottom-Right-Square to read info in the Text Box        &copy 145128
+</p>
+</H2>
+<H5 align="right">
+<a href="https://en.wikipedia.org/wiki/Western_hoolock_gibbon">
+Go To Wikipedia Page For More ...
+</a>
+</H5>
+
+<div id="footer" style="position:relative">										<!--footer-->
+<button id="id4">CONTACT</button><br/><br/>
+<span style="font-size:0.7em">Dated 9/12/16.</span>
+</div>
+
+</div>
+
+</BODY>
+</HTML>
